789acb922a72f34b77bcd7af6ab80f730eedee1c
Wahidul30 <e191021@ugrad.iiuc.ac.bd>
2022-02-21 10:52:01 +0600

    fixed something

--- destructuring.js
+++ destructuring.js
@@ -1,8 +1,8 @@
-const company = { name: 'Think wide', type: 'Creativity', rating: 8.2, employeeNumber: 39 };
-const { type, employeeNumber } = company;
+// object destructuring 
+const { type, employeeNumber } = { name: 'Think wide', type: 'Creativity', rating: 8.2, employeeNumber: 39 };
 console.log(type);
 console.log(type, employeeNumber);
-// another example using object
+// another example of object destructuring
 const elements = {
   mostlyUse: 'laptop', wearing: 't-shirt',
   bottle: { brand: 'fresh', price: 20, quantity: '1L' },
@@ -16,14 +16,14 @@ console.log(name, price, color);
 const { medium, dark } = elements.phone.color;
 console.log(medium, dark);
 
-// another example using Array
+// Array destructuring 
 const [a, b] = [12, 67, 38];
 console.log(a, b);
 // another 
 const arr1 = [34, 7, 16, 62, 11, 83, 48];
-const [first, second] = arr1;
-console.log(first, second);
-// another
+const [first, second, third, fourth] = arr1;
+console.log(first, third, fourth, second);
+// another array destructuring 
 const arr2 = ['Wahidul', 'Moshu', 'Morsheda', 'Sarzil'];
 const [position1, position2, position3] = arr2;
 console.log(position1, position3, position2);
